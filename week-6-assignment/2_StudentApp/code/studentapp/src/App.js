c3d00ebbe9410d76c2fee73de4649a2b435c3133
Saanchita123 <sanchitad648@gmail.com>
2025-07-24 21:14:43 +0530

    adding week 6 hands on solutions

---
+++ week-6-assignment/2_StudentApp/code/studentapp/src/App.js
@@ -0,0 +1,16 @@
+import {Home} from './Component/Home';
+import {About} from './Component/About';
+import { Contact } from './Component/Contact';
+import './App.css'
+
+function App()
+{
+  return (
+    <div>
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
+  )
+}
+export default App;
---
+++ week-6-assignment/2_StudentApp/code/studentapp/src/Component/Contact.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+export class Contact extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h3>
+                    Welcome to the contact Page of the Student Management System.
+                </h3>
+                </div>
+        )
+    }
+}
